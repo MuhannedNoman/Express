47e9501c5f81e567bfcf72b811ab227260853587
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-01-30 06:58:28 +0300

    Get data from user using route params

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/',(request,response)=>{
-response.send('Hello from Express server');
+app.get("/", (request, response) => {
+  response.send("Hello from Express server");
 });
 
-app.get('/test',(request,response)=>{
-    response.send('Your in test route');
-    });
+app.get("/test", (request, response) => {
+  response.send("Your in test route");
+});
+
+app.get("/test/:name/:age", (request, response) => {
+  response.send(request.params.name + " : " + request.params.age)
+});
 
 app.listen(3005);
